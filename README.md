efe6cf17fe59a3e324c86d75c883c4c649b5fd7f
Brogramerr <fagbolade006@gmail.com>
2023-09-15 12:51:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Uniswap V3 Event Listener
+
+This Node.js application listens for events emitted by a Uniswap V3 pool smart contract on the Ethereum blockchain and stores those events in a MongoDB database. It can be used to monitor and analyze blockchain data related to decentralized finance (DeFi) operations.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+-   Node.js installed (https://nodejs.org/)
+-   MongoDB installed and running locally (https://docs.mongodb.com/manual/installation/)
+
+## Getting Started
+
+1.  Clone this repository to your local machine:
+
+        ```bash
+        git clone https://github.com/Brogramerr/UniswapV3-Event-Listener.git
+        ```
+
+    Running the Event Listener
+
+2.  Start MongoDB Server :
+
+```mongod
+
+```
+
+3. Restore Packages:
+
+```npm install
+
+```
+
+4. Run your Application:
+
+```node listener.js
+
+```
+
+In this README file, replace placeholders like `YOUR_POOL_CONTRACT_ADDRESS` and `YOUR_ETHEREUM_NODE_URL` with the specific Ethereum contract address and Ethereum node WebSocket URL you intend to use.
+
+Feel free to modify the README to include additional details or instructions specific to your project or deployment environment.
---
+++ listener.js
@@ -0,0 +1,46 @@
+const { Web3 } = require("web3")
+const { MongoClient } = require("mongodb")
+require("dotenv").config()
+
+const POOL_CONTRACT_ADDRESS = process.env.POOL_CONTRACT_ADDRESS
+//const ETHEREUM_NODE_URL = process.env.ETHEREUM_NODE_URL
+const ETHEREUM_NODE_URL = "wss://eth-mainnet.g.alchemy.com/v2/LltQfNYyYo77QRM50bKYHQAMA3A2d4ny"
+
+if (!ETHEREUM_NODE_URL) {
+    console.error("ETHEREUM_NODE_URL is not defined. Please set it.")
+    process.exit(1)
+}
+
+const web3 = new Web3(ETHEREUM_NODE_URL)
+const poolContractAddress = POOL_CONTRACT_ADDRESS
+const contractABI = require("./UniswapV3PoolABI.json")
+
+const client = new MongoClient("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+async function main() {
+    try {
+        await client.connect()
+        console.log("Connected to MongoDB")
+        const db = client.db("uniswap_events")
+        const collection = db.collection("pool_events")
+
+        const poolContract = new web3.eth.Contract(contractABI, poolContractAddress)
+
+        poolContract.events
+            .allEvents()
+            .on("data", async (event) => {
+                console.log("Received event:", event)
+                await collection.insertOne(event)
+            })
+            .on("error", (error) => {
+                console.error("Error:", error)
+            })
+    } catch (err) {
+        console.error("Error:", err)
+    }
+}
+
+main()
